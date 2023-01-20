356bc7c00fe1c8401e47b24795418e5ab03206d3
Samara Carvalho <samara.coelho@codenapp.com>
2023-01-20 09:21:34 -0300

    Cria a mensagem "carregando filmes" para quando a internet estiver mais lenta

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,45 +1,58 @@
-import { useEffect, useState } from 'react'
-import api from '../../services/api'
-import { Link } from 'react-router-dom'
-import './home.css'
- // URL DA API: https://api.themoviedb.org/3/movie/now_playing?api_key=cd5402cb28c96319d1c616da9c152817&language=pt-BR
-
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import { Link } from "react-router-dom";
+import "./home.css";
+// URL DA API: https://api.themoviedb.org/3/movie/now_playing?api_key=cd5402cb28c96319d1c616da9c152817&language=pt-BR
 
 function Home() {
+  const [filmes, setFilmes] = useState([]);
+    const[loading, setLoading] = useState(true);
 
-    const [filmes, setFilmes] = useState([])
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "cd5402cb28c96319d1c616da9c152817",
+          language: "pt-BR",
+          page: 1,
+        },
+      });
 
-    useEffect(() => {
-        async function loadFilmes() {
-            const response = await api.get("movie/now_playing", {
-                params: {
-                    api_key:"cd5402cb28c96319d1c616da9c152817",
-                    language: "pt-BR",
-                    page: 1,
-                }
-            })
+      // console.log(response.data.results.slice(0,10))
+      setFilmes(response.data.results.slice(0, 10));
+      setLoading(false);
+    }
 
-            // console.log(response.data.results.slice(0,10))
-            setFilmes(response.data.results.slice(0,10))
-        }
+    loadFilmes();
+  }, []);
 
-        loadFilmes();
-    }, [])
+
+if(loading) {
     return (
-        <div className='container'> 
-            <div className='lista-filmes'>
-                {filmes.map((filme) => {
-                    return(
-                        <article key={filme.id}>
-                            <strong>{filme.title}</strong>
-                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
-                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
-                        </article>
-                    )
-                })}
-            </div>
+        <div className="loading">
+            <h2>Carregando filmes...</h2>
         </div>
     )
+}
+
+  return (
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+                alt={filme.title}
+              />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
